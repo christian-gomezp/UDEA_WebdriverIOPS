a43fe055dc1fac670f56c94049dc368d09e4008d
christian-gomezp <christian.gomezp@udea.edu.co>
2026-05-26 21:14:37 -0500

    Update feature file patterns and step definitions

--- wdio.conf.js
+++ wdio.conf.js
@@ -23,7 +23,7 @@ export const config = {
     // will be called from there.
     //
     specs: [
-        './features/**/login.feature'
+        './features/**/*.feature'
     ],
     // Patterns to exclude.
     exclude: [
@@ -141,8 +141,10 @@ export const config = {
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
         // <string[]> (file/dir) require files before executing features
-        require: ['./features/step-definitions/steps.js'],
-        // <boolean> show full backtrace for errors
+        require: [
+        "./features/step-definitions/steps.js",
+        "./features/step-definitions/transferSteps.js",
+        ],        // <boolean> show full backtrace for errors
         backtrace: false,
         // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
         requireModule: [],
